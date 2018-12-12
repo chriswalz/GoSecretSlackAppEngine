e72ee62536b8886ef569894bbb030d931f6d19e5
chriswalz <zlaw777@gmail.com>
2018-12-11 23:34:50 -0500

    refactor

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
-Running on App Engine 
+#Running on App Engine 
 
 Open cloud console 
+
 git clone 
+
 go run . (to test)
+
 gcloud app deploy (to deploy)
--- main.go
+++ main.go
@@ -25,5 +25,5 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-	fmt.Fprint(w, "Hello, World!")
+	fmt.Fprint(w, "Go Secret Slack!")
 }
